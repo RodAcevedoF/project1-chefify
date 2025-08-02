fb95d102a42f09053d0d0f131857574a3a57c0d9
Rodrigo Acevedo <raacevedof@gmail.com>
2025-08-02 22:08:49 +0200

    fix: change model at openai.helper

--- src/utils/openai.helper.ts
+++ src/utils/openai.helper.ts
@@ -5,7 +5,7 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 export async function getSuggestedRecipe(prompt: string) {
   const response = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
+    model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: prompt }],
     temperature: 0.8,
   });
