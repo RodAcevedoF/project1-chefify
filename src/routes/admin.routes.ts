ad9b759389cb6c32195bd347ec0a0f796b062e18
Rodrigo Acevedo <racevedo@bluemedlar.io>
2025-09-19 20:16:41 +0200

    wip

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,13 +1,19 @@
-import { Router } from "express";
-import { AdminController } from "../controllers";
-import { authenticate } from "../middlewares/authenticate";
-import { uploadDataFile } from "../middlewares/uploadDataFile";
+import { Router } from 'express';
+import { AdminController, UserController } from '../controllers';
+import { authenticate } from '../middlewares/authenticate';
+import { uploadDataFile } from '../middlewares/uploadDataFile';
+import { validateBody } from '../middlewares/validateBody';
+import { UserInputSchema } from '../schemas';
 const router = Router();
 
-router.use(authenticate(["admin"]));
+router.use(authenticate(['admin']));
 
-router.post("/recipes", uploadDataFile, AdminController.importRecipes);
-router.post("/ingredients", uploadDataFile, AdminController.importIngredients);
-router.get("/users", AdminController.getUsers);
+router.post('/', validateBody(UserInputSchema), UserController.create);
+router.post('/recipes', uploadDataFile, AdminController.importRecipes);
+router.post('/ingredients', uploadDataFile, AdminController.importIngredients);
+router.get('/users', AdminController.getUsers);
+router.get('/:id', UserController.getById);
+router.patch('/:id', UserController.update);
+router.delete('/:id', UserController.delete);
 
 export default router;
