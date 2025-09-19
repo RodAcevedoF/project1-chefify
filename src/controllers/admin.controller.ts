c642e869d5023ff57035d1d79a83c52c9a1c1a39
Rodrigo Acevedo <racevedo@bluemedlar.io>
2025-09-19 20:39:47 +0200

    chore remove unnecesary try block

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,73 +1,60 @@
-import type { Request, Response, NextFunction } from "express";
-import { ForbiddenError, BadRequestError, NotFoundError } from "../errors";
-import { successResponse, readRecipeCsv, readIngredient } from "../utils";
-import { IngredientService, RecipeService, UserService } from "../services";
+import type { Request, Response } from 'express';
+import { ForbiddenError, BadRequestError, NotFoundError } from '../errors';
+import { successResponse, readRecipeCsv, readIngredient } from '../utils';
+import { IngredientService, RecipeService, UserService } from '../services';
 
 export const AdminController = {
-  async importRecipes(req: Request, res: Response, next: NextFunction) {
-    try {
-      if (req.user?.role !== "admin") {
-        throw new ForbiddenError("Only admins can import recipes");
-      }
+	async importRecipes(req: Request, res: Response) {
+		if (req.user?.role !== 'admin') {
+			throw new ForbiddenError('Only admins can import recipes');
+		}
 
-      const file = req.file;
-      if (!file) throw new BadRequestError("No file uploaded");
+		const file = req.file;
+		if (!file) throw new BadRequestError('No file uploaded');
 
-      const records = await readRecipeCsv(file);
-      if (!records.length) {
-        throw new BadRequestError("No valid recipes found in file");
-      }
+		const records = await readRecipeCsv(file);
+		if (!records.length) {
+			throw new BadRequestError('No valid recipes found in file');
+		}
 
-      const inserted = await RecipeService.importRecipesFromCsv(records);
+		const inserted = await RecipeService.importRecipesFromCsv(records);
 
-      return successResponse(
-        res,
-        {
-          message: `Imported ${inserted.length} recipes successfully`,
-          data: inserted
-        },
-        201
-      );
-    } catch (error) {
-      next(error);
-    }
-  },
-  async importIngredients(req: Request, res: Response, next: NextFunction) {
-    try {
-      if (req.user?.role !== "admin") {
-        throw new ForbiddenError("Only admins can import recipes");
-      }
+		return successResponse(
+			res,
+			{
+				message: `Imported ${inserted.length} recipes successfully`,
+				data: inserted,
+			},
+			201,
+		);
+	},
+	async importIngredients(req: Request, res: Response) {
+		if (req.user?.role !== 'admin') {
+			throw new ForbiddenError('Only admins can import recipes');
+		}
 
-      const file = req.file;
-      if (!file) throw new BadRequestError("No file uploaded");
+		const file = req.file;
+		if (!file) throw new BadRequestError('No file uploaded');
 
-      const records = await readIngredient(file);
-      if (!records.length) {
-        throw new BadRequestError("No valid recipes found in file");
-      }
+		const records = await readIngredient(file);
+		if (!records.length) {
+			throw new BadRequestError('No valid recipes found in file');
+		}
 
-      const inserted =
-        await IngredientService.importIngredientsFromCsv(records);
+		const inserted = await IngredientService.importIngredientsFromCsv(records);
 
-      return successResponse(
-        res,
-        {
-          message: `Imported ${inserted.length} ingredients}`,
-          data: inserted
-        },
-        201
-      );
-    } catch (error) {
-      next(error);
-    }
-  },
-  async getUsers(req: Request, res: Response, next: NextFunction) {
-    try {
-      const users = await UserService.getAllUsers();
-      if (!users) throw new NotFoundError("No users found");
-      successResponse(res, users, 200);
-    } catch (error) {
-      next(error);
-    }
-  }
+		return successResponse(
+			res,
+			{
+				message: `Imported ${inserted.length} ingredients}`,
+				data: inserted,
+			},
+			201,
+		);
+	},
+	async getUsers(req: Request, res: Response) {
+		const users = await UserService.getAllUsers();
+		if (!users) throw new NotFoundError('No users found');
+		successResponse(res, users, 200);
+	},
 };
