a1f77203869e5ea2751f13f358d14d2898753f06
Rodrigo Acevedo <raacevedof@gmail.com>
2025-10-11 21:54:53 +0200

    fix guard

--- src/middlewares/ingredient.guard.ts
+++ src/middlewares/ingredient.guard.ts
@@ -1,8 +1,11 @@
-import { IngredientService } from "../services";
-import { ownership } from "./ownership";
+import { IngredientService } from '../services';
+import { ownership } from './ownership';
 
 export const ingredientGuard = ownership({
-  findById: () => IngredientService.getIngredientById,
-  field: "createdBy",
-  resourceName: "ingredient",
+	findById: () => async (id: string) => {
+		const result = IngredientService.getIngredients({ id });
+		return Array.isArray(result) ? result[0] : result;
+	},
+	field: 'createdBy',
+	resourceName: 'ingredient',
 });
