e279327c1b012dc0eae099f10f075adec29b1a01
Rodrigo Acevedo <raacevedof@gmail.com>
2025-10-11 21:55:06 +0200

    fix tests

--- tests/mocks/seedMockRecipes.ts
+++ tests/mocks/seedMockRecipes.ts
@@ -1,44 +1,44 @@
-import type { RecipeInput } from "../../src/schemas";
+import type { RecipeInput } from '../../src/schemas';
 
 export const dummyRecipes = [
-  {
-    title: "Garlic Soup",
-    userId: "65c123456789456123456784",
-    categories: ["Soups"],
-    ingredients: [
-      {
-        ingredient: "64b111111111111111111124",
-        quantity: 3,
-      },
-    ],
-    instructions: [
-      "Chop garlic",
-      "Fry in abundant oil",
-      "Prepare broth",
-      "Cook for 1 hour",
-    ],
-    servings: 2,
-    prepTime: 70,
-    utensils: ["Pan", "Pot"],
-  },
-  {
-    title: "Creamy garlic",
-    userId: "65c123456789456123456785",
-    categories: ["Creams"],
-    ingredients: [
-      {
-        ingredient: "64b111111111111111111124",
-        quantity: 3,
-      },
-    ],
-    instructions: [
-      "Boil garlics at least 1 hour",
-      "Mix with heavy cream",
-      "Keep in the fridge",
-      "Let it sit for 1 hour",
-    ],
-    servings: 10,
-    prepTime: 80,
-    utensils: ["Pot", "Food processor"],
-  },
+	{
+		title: 'Garlic Soup',
+		userId: '65c123456789456123456784',
+		categories: ['soups'],
+		ingredients: [
+			{
+				ingredient: '64b111111111111111111124',
+				quantity: 3,
+			},
+		],
+		instructions: [
+			'Chop garlic',
+			'Fry in abundant oil',
+			'Prepare broth',
+			'Cook for 1 hour',
+		],
+		servings: 2,
+		prepTime: 70,
+		utensils: ['Pan', 'Pot'],
+	},
+	{
+		title: 'Creamy garlic',
+		userId: '65c123456789456123456785',
+		categories: ['soups'],
+		ingredients: [
+			{
+				ingredient: '64b111111111111111111124',
+				quantity: 3,
+			},
+		],
+		instructions: [
+			'Boil garlics at least 1 hour',
+			'Mix with heavy cream',
+			'Keep in the fridge',
+			'Let it sit for 1 hour',
+		],
+		servings: 10,
+		prepTime: 80,
+		utensils: ['Pot', 'Food processor'],
+	},
 ] satisfies RecipeInput[];
--- tests/repositories/ingredient.repository.test.ts
+++ tests/repositories/ingredient.repository.test.ts
@@ -1,57 +1,61 @@
-import { describe, it, expect } from "bun:test";
-import { setupRepoTestDB } from "../mocks/repoTestSetup";
-import { IngredientRepository } from "../../src/repositories";
-import type { IngredientInput } from "../../src/schemas";
+import { describe, it, expect } from 'bun:test';
+import { setupRepoTestDB } from '../mocks/repoTestSetup';
+import { IngredientRepository } from '../../src/repositories';
+import type { IngredientInput } from '../../src/schemas';
 
 const dummyIngredient: IngredientInput = {
-  name: "Garlic",
-  unit: "cloves",
+	_id: '680f1f4e4d3c2b1a2b3c4d5e',
+	name: 'Garlic',
+	unit: 'cloves',
 };
 
 setupRepoTestDB();
 
-describe("UserRepository CRUD operations", () => {
-  it("should create a user", async () => {
-    const ingredient = await IngredientRepository.create(dummyIngredient);
-    expect(ingredient).toBeDefined();
-    expect(ingredient.name).toBe(dummyIngredient.name);
-  });
+describe('IngredientRepository CRUD operations', () => {
+	it('should create an ingredient', async () => {
+		await IngredientRepository.create(dummyIngredient);
+		const ingredient = await IngredientRepository.findById(
+			dummyIngredient._id!,
+		);
+		expect(ingredient).toBeDefined();
+		expect(ingredient).not.toBeNull();
+		expect(ingredient!.name).toBe(dummyIngredient.name);
+	});
 
-  it("should find a user by ID", async () => {
-    const created = await IngredientRepository.create(dummyIngredient);
-    if (!created._id) throw new Error("Data ID not found");
+	it('should find an ingredient by ID', async () => {
+		await IngredientRepository.create(dummyIngredient);
+		const ingredient = await IngredientRepository.findById(
+			dummyIngredient._id!,
+		);
+		expect(ingredient).toBeDefined();
+		expect(ingredient).not.toBeNull();
+		// compare ids as strings
+		expect(ingredient!._id.toString()).toBe(dummyIngredient._id!);
+	});
 
-    const found = await IngredientRepository.findById(created._id.toString());
+	it('should update an ingredient partially or fully', async () => {
+		await IngredientRepository.create(dummyIngredient);
+		const updated = (await IngredientRepository.updateById(
+			dummyIngredient._id!,
+			{
+				name: 'Black Garlic',
+			},
+		)) as any;
 
-    expect(found).toBeDefined();
-    expect(found!._id.toString()).toBe(created._id.toString());
-  });
+		expect(updated).toBeDefined();
+		expect((updated as any).name).toBe('Black Garlic');
+	});
 
-  it("should update a user partially or fully", async () => {
-    const created = await IngredientRepository.create(dummyIngredient);
-    if (!created._id) throw new Error("Data ID not found");
-    const updated = await IngredientRepository.updateById(
-      created._id.toString(),
-      {
-        name: "Black Garlic",
-      }
-    );
+	it("should delete an ingredient and validate it's gone", async () => {
+		await IngredientRepository.create(dummyIngredient);
 
-    expect(updated).toBeDefined();
-    expect(updated!.name).toBe("Black Garlic");
-  });
+		const deleted = (await IngredientRepository.deleteById(
+			dummyIngredient._id!,
+		)) as any;
+		expect(deleted).toBeDefined();
+		expect((deleted as any)._id.toString()).toBe(dummyIngredient._id!);
 
-  it("should delete a user and validate it's gone", async () => {
-    const created = await IngredientRepository.create(dummyIngredient);
-    if (!created._id) throw new Error("Data ID not found");
-
-    const deleted = await IngredientRepository.deleteById(
-      created._id.toString()
-    );
-    expect(deleted).toBeDefined();
-    expect(deleted!._id.toString()).toBe(created._id.toString());
-
-    const refetch = await IngredientRepository.findById(created._id.toString());
-    expect(refetch).toBeNull();
-  });
+		const refetch = await IngredientRepository.findById(dummyIngredient._id!);
+		expect(refetch).toBeNull();
+	});
 });
