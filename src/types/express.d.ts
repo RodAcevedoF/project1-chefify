bb8b4f154ef29d27ec0118efad4b9a4f165d6dc7
Rodrigo Acevedo <raacevedof@gmail.com>
2025-11-02 21:51:34 +0100

    feat extend req for sessions

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,12 @@
-import { JWTPayload } from "../schemas";
+import { JWTPayload } from '../schemas';
+import type { Session, SessionData } from 'express-session';
 
 declare global {
-  namespace Express {
-    interface Request {
-      user?: JWTPayload;
-    }
-  }
+	namespace Express {
+		interface Request {
+			user?: JWTPayload;
+			session?: Session & SessionData & { user?: JWTPayload };
+			sessionID?: string;
+		}
+	}
 }
